1551441137f82fa503836c705f4daf6c0d8a9898
Udit Karode <udit.karode@gmail.com>
2021-10-03 23:19:11 +0530

    [rm:treewide] remove useless imports

--- app/Trans.hs
+++ app/Trans.hs
@@ -1,32 +1,31 @@
 module Trans where
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Trans.Except (ExceptT)
 import Data.Text (Text)
 import Servant (Handler)
 
-newtype HandlerT a = HandlerT {runHandlerT :: Handler (Either Text a)}
+newtype HandlerT e a = HandlerT {runHandlerT :: Handler (Either e a)}
 
-liftHT :: Either Text a -> HandlerT a
+liftHT :: Either e a -> HandlerT e a
 liftHT v = HandlerT $ pure v
 
-instance MonadIO HandlerT where
+instance MonadIO (HandlerT e) where
   liftIO = liftIO
 
-instance Functor HandlerT where
+instance Functor (HandlerT e) where
   fmap f (HandlerT val) = HandlerT $ do
     val >>= \case
       Left err -> pure (Left err)
       Right v -> pure (Right (f v))
 
-instance Applicative HandlerT where
+instance Applicative (HandlerT e) where
   pure x = HandlerT (pure (Right x))
   HandlerT fn <*> HandlerT v = HandlerT $ do
     fn <- fn
     v <- v
     pure $ fn <*> v
 
-instance Monad HandlerT where
+instance Monad (HandlerT e) where
   (HandlerT val) >>= fn = HandlerT $ do
     val >>= \case
       Left err -> pure (Left err)
