b0a411a7dd49eca85a09098bda132734c10a069c
Udit Karode <udit.karode@gmail.com>
2021-10-15 20:15:42 +0530

    [feat:Main] send back Status JSON on exception

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Exception (SomeException)
 import Control.Monad.IO.Class (liftIO)
+import Data.Aeson (encode)
 import Data.Proxy
 import Data.Text (Text)
 import Network.HTTP.Types.Status (status500)
@@ -9,17 +10,15 @@ import Network.Wai (Response, responseLBS)
 import Network.Wai.Handler.Warp
 import Routes
 import Servant
-import Trans
 import Types
 import Wrappers (fcRouteWrapper, rootRouteWrapper)
 
-myHandler :: SomeException -> Response
-myHandler se = do
-  -- liftIO $ putStrLn ""
-  responseLBS status500 [] "HERE WE ARE"
+errHandler :: SomeException -> Response
+errHandler se = do
+  responseLBS status500 [] (encode $ Status False "Something went wrong!")
 
-mySettings :: Settings
-mySettings = setOnExceptionResponse myHandler $ setPort 3002 defaultSettings
+ecSettings :: Int -> Settings
+ecSettings port = setOnExceptionResponse errHandler $ setPort port defaultSettings
 
 main :: IO ()
 main = do
@@ -33,4 +32,4 @@ main = do
           )
   putStrLn $ "Starting server running on port " <> show port
 
-  run port app
+  runSettings (ecSettings port) app
