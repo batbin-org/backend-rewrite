d8a310236e9117cb6c0bfae37b06e10e1729b397
Udit Karode <udit.karode@gmail.com>
2022-06-18 21:09:45 +0530

    fix(Routes): resolve ambiguous types

--- app/Routes.hs
+++ app/Routes.hs
@@ -6,13 +6,13 @@ import Data.ByteString.Char8 (readInt)
 import qualified Data.ByteString.Char8 as BS
 import Data.ByteString.UTF8 as B (fromString)
 import Data.Text as T (Text, isPrefixOf, length, unpack)
-import Text.Regex.TDFA ((=~))
 import Data.Text.IO as TIO (readFile, writeFile)
 import Database (getRandomName, markAsTaken)
 import Database.Redis (runRedis)
 import qualified Database.Redis as R
 import Database.SQLite.Simple (Connection)
 import System.Directory (doesFileExist)
+import Text.Regex.TDFA ((=~))
 import Trans (HandlerT, liftHT, succeed)
 import Types (Status (Status))
 import Utils
@@ -33,9 +33,7 @@ fetch :: Connection -> Cli -> Text -> HandlerT Text
 fetch conn cli id = do
   let path = pastesDir cli <> "/" <> unpack id
 
-  valid <-
-    (id =~ alphabets)
-      <?> Replace "Paste ID cannot contain non-alphabet characters!"
+  valid <- (id =~ alphabets :: Bool) <!?> Replace "Paste ID cannot contain non-alphabet characters!"
 
   liftIO (doesFileExist path)
     >>= (<?!>) (Replace "The provided paste ID does not exist!")
